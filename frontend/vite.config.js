ed9d483893e61f0dd0e338c83e5a82d03718a2e0
Promatics Technologies Pvt Ltd, Ludhiana <promatics.honeykumar@gmail.com>
2025-05-17 14:09:27 +0530

    changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://hr-dashboard-assignment.onrender.com/',
         changeOrigin: true,
         secure: false,
       },
